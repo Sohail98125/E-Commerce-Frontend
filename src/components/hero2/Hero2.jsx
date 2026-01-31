e03960d5781a99353e3b1708c96877ea73d9fd42
unknown <sohail98125@example.com>
2026-01-31 18:32:48 +0530

    responsive layout

--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -6,59 +6,58 @@ import './Hero2.css'
 import { Link, useNavigate } from 'react-router-dom';
 
 
-const Hero2= () => {
-    const settings = {
-    dots: true,         
-    infinite:true,      
-    speed: 400,       
-    slidesToShow: 1,    
-    slidesToScroll: 1,  
-    autoplay: true,      
-    autoplaySpeed: 1500,
-}
-const navigate = useNavigate();
+const Hero2 = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  }
+  const navigate = useNavigate();
 
   return (
     <div className='heroine'>
       <div className='hero'>
         <Slider {...settings}>
-            <div className='hero2'>
-              <h2>ALL NEW ARRIVALS</h2>
-              <div id='new2'>
+          <div className='hero2'>
+            <h2>ALL NEW ARRIVALS</h2>
+            <div id='new2'>
               <p>NEW</p>
               <p>COLLECTIONS</p>
               <p>FOR EVERYONE</p>
-              </div>
-             <div className= "hero2-explore" onClick={() => navigate("/women")}>EXPLORE MORE</div> 
-                
             </div>
-            <div className='hero1'>
-              <h2>ALL NEW ARRIVALS</h2>
-              <div id='new1'>
-               <p>NEW</p>
+            <button className="hero2-explore" onClick={() => navigate("/women")}>EXPLORE MORE</button>
+          </div>
+          <div className='hero1'>
+            <h2>ALL NEW ARRIVALS</h2>
+            <div id='new1'>
+              <p>NEW</p>
               <p>COLLECTIONS</p>
               <p>FOR EVERYONE</p>
-              </div>
-             <div className= "hero1-explore" onClick={() => navigate("/women")}>EXPLORE MORE</div>    
+              <button className="hero1-explore" onClick={() => navigate("/women")}>EXPLORE MORE</button>
             </div>
-            <div className='hero3'>
-              <h2>ALL NEW ARRIVALS</h2>
-              <div id='new3'>     
-               <p>NEW</p>
+          </div>
+          <div className='hero3'>
+            <h2>ALL NEW ARRIVALS</h2>
+            <div id='new3'>
+              <p>NEW</p>
               <p>COLLECTIONS</p>
               <p>FOR EVERYONE</p>
-              </div>
-             <div className= "hero3-explore" onClick={() => navigate("/women")}>EXPLORE MORE</div> 
             </div>
-            <div className='hero4'>
-              <h2>ALL NEW ARRIVALS</h2>
-              <div id='new4'>     
-               <p>NEW</p>
+            <button className="hero3-explore" onClick={() => navigate("/women")}>EXPLORE MORE</button>
+          </div>
+          <div className='hero4'>
+            <h2>ALL NEW ARRIVALS</h2>
+            <div id='new4'>
+              <p>NEW</p>
               <p>COLLECTIONS</p>
               <p>FOR EVERYONE</p>
-              </div>
-                 <div className= "hero4-explore" onClick={() => navigate("/women")}>EXPLORE MORE</div> 
             </div>
+            <button className="hero4-explore" onClick={() => navigate("/women")}>EXPLORE MORE</button>
+          </div>
         </Slider>
       </div>
     </div>
